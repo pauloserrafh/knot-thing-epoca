ba31cffe8daf73b3076249931f66f5afa03f0784
Paulo Serra Filho <ptvsf@cesar.org.br>
2017-10-23 17:20:10 -0300

    Include libraries and define pins
    Include the KNoTThing library and define the pins that will be used
    for each sensor/actuator.

---
+++ epoca.ino
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2016, CESAR.
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license. See the LICENSE file for details.
+ *
+ */
+
+/*
+ * The default behavior for a Thing is to send data every 30 seconds.
+ * To change its behavior on the firmware side, use the function
+ * registerDefaultConfig(). See the documentation and lib examples.
+ */
+
+#include <KNoTThing.h>
+
+#define LIGHT_BULB_PIN      2
+#define LIGHT_BULB_ID       1
+#define LIGHT_BULB_NAME     "Light bulb"
+
+KNoTThing thing;
+
+
+void setup()
+{
+
+}
+
+void loop()
+{
+
+}
